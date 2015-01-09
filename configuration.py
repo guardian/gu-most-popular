f5b4a8085de2b276669595c45210e2afa4b55bde
Robert Rees <robert.rees@guardian.co.uk>
2015-01-09 17:26:40 +0000

    Switches the app to using configuration for the content api details

---
+++ configuration.py
@@ -0,0 +1,16 @@
+from models import Configuration
+
+def lookup(key, default):
+	results = Configuration.query(Configuration.key == key)
+
+	if not results.iter().has_next():
+		return default
+
+	key_value = results.iter().next().value
+
+	return key_value
+
+def create(key, value):
+	config = Configuration(id=key, key=key, value=value)
+	config.put()
+	return config
--- content_api.py
+++ content_api.py
@@ -5,26 +5,27 @@ import logging
 from google.appengine.api.urlfetch import fetch
 from google.appengine.api import memcache
 
-CONTENT_API_HOST = 'content.guardianapis.com'
+import configuration
+
+CONTENT_API_HOST = configuration.lookup('CONTENT_API_HOST', 'content.guardianapis.com')
 
 def content_id(url):
 	parsed_url = urlparse.urlparse(url)
 	return parsed_url.path
 
 def read(content_id, params = None):
-	client = memcache.Client()
 
 	url = "http://%s%s" % (CONTENT_API_HOST, content_id)
 
 	if params:
-		cached_key = client.get('API_KEY')
+		cached_key = configuration.lookup('API_KEY', 'gu-most-popular')
 		if not 'api-key' in params and cached_key:
 			params['api-key'] = cached_key
 		url = url + "?" + urllib.urlencode(params)
 
 	#logging.debug(url)
 
-	cached_data = client.get(url)
+	cached_data = memcache.get(url)
 
 	if cached_data: return cached_data
 
@@ -34,6 +35,6 @@ def read(content_id, params = None):
 		logging.warning("Content API read failed: %d" % result.status_code)
 		return None
 
-	client.set(url, result.content, time = 60 * 15)
+	memcache.set(url, result.content, time = 60 * 15)
 
 	return result.content
